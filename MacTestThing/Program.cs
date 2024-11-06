c5bd84411ba9d7ce185e16652e59c7ca7e6b0a84
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-11-06 15:34:24 +0100

    Fixed smoll issue

--- MacTestThing/Program.cs
+++ MacTestThing/Program.cs
@@ -3,5 +3,5 @@ using Hexa.NET.ImGui.Widgets.Dialogs;
 
 foreach (var item in FileUtilities.EnumerateEntriesOSX(AppDomain.CurrentDomain.BaseDirectory, "*", SearchOption.TopDirectoryOnly))
 {
-    Console.WriteLine(item);
+    Console.WriteLine(item.Path);
 }
