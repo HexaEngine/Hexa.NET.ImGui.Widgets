4aed86a14443daaed79fc580df68b08d0ede314b
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-11-08 13:41:25 +0100

    Added unit tests.

---
+++ Hexa.NET.ImGui.Widgets.Tests/FileUtilitiesTests.cs
@@ -0,0 +1,30 @@
+namespace Hexa.NET.ImGui.Widgets.Tests
+{
+    using Hexa.NET.ImGui.Widgets.Dialogs;
+
+    [TestFixture]
+    public class FileUtilitiesTests
+    {
+        [Test]
+        [Platform(Include = "MacOsX", Reason = "This test is only applicable on macOS.")]
+        public void EnumerateEntriesOSXTest()
+        {
+            // Arrange
+            string testDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            foreach (var entry in FileUtilities.EnumerateEntriesOSX(testDirectory, "*", SearchOption.TopDirectoryOnly))
+            {
+                var path = entry.Path.ToString();
+                string fileName = Path.GetFileName(path);
+                Assert.Multiple(() =>
+                {
+                    Assert.That(string.IsNullOrEmpty(path), Is.False, "Path should not be empty");
+                    Assert.That(string.IsNullOrEmpty(fileName), Is.False, "File name should not be empty");
+                });
+
+                // Optional: Print the path and file name for verification
+                Console.WriteLine($"Path: {entry.Path}, File Name: {fileName}");
+            }
+        }
+    }
+}
